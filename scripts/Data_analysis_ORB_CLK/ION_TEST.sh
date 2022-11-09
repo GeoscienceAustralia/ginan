3c0fd0927abe8a72cb475c0bb7d19cd750983e69
VDuong <vduong@hgnss.com>
2022-11-09 13:11:26 -0700

    orbit comparison on doy 352

--- scripts/Data_analysis_ORB_CLK/ION_TEST.sh
+++ scripts/Data_analysis_ORB_CLK/ION_TEST.sh
@@ -9,7 +9,7 @@ export PYTHON_FILES=/data/ginan/scripts
 
 echo "//**********************Orbit comparison (Integration)*************************//"
 # python3 $PYTHON_FILES/sp3_compare.py $STD_FILE/COD0MGXFIN_20222230000_01D_05M_ORB.SP3 $STD_FILE/GBM0MGXRAP_20222230000_01D_05M_ORB_GE.SP3 --ylims -50 50 -hlm ECF 
-python3 $PYTHON_FILES/sp3_compare.py $STD_FILE/COD0MGXFIN_20222800000_01D_05M_ORB.SP3 $STD_FILE/igs22305.sp3 --ylims -20 20 -hlm ECF 
+python3 $PYTHON_FILES/sp3_compare.py $STD_FILE/COD0MGXFIN_20213520000_01D_05M_ORB.SP3 $STD_FILE/cnt21886_GEC.sp3 --ylims -10 10 -hlm ECF 
 # echo "//***************************Clock comparison**********************************//"
 # python3 $PYTHON_FILES/compareClock.py --standard $STD_FILE/COD0MGXFIN_20222230000_01D_30S_CLK.CLK --test $STD_FILE/GBM0MGXRAP_20222230000_01D_30S_CLK.CLK
 
