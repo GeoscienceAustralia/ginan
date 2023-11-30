dae305d651419f9abf621988073effd77f58540c
Andrew Cleland <andrew.cleland3@gmail.com>
2023-12-01 04:53:15 +1000

    Write config to "network" directory

--- scripts/generate_yaml.py
+++ scripts/generate_yaml.py
@@ -2,17 +2,20 @@ from pathlib import Path
 from auto_generate_yaml import out_pea_yaml
 
 
-out_pea_yaml(
-    start_epoch = "",
-    end_epoch = "",
-    template_path = Path('templates/auto_template.yaml'),
-    product_dir = Path('/var/products'),
-    data_dir = Path('/var/data'),
-    pea_out_dir = Path('/var/outputs'),
-    config_out_dir = Path('/var/generated_templates'),
-    relative_to_dir = None,
-    trop_model="gpt2",
-    trop_dir = None,
-    enable_mongo = False,
-    overrides=(),
-)
+def write_yaml(network_name):
+    ginan_dir = Path('/var/ginan/')
+    network_dir = ginan_dir / network_name
+    out_pea_yaml(
+        start_epoch = "",
+        end_epoch = "",
+        template_path = Path('templates/auto_template.yaml'),
+        product_dir = network_dir, # / 'products',
+        data_dir = network_dir, # / 'data'
+        pea_out_dir = network_dir, # / 'outputs',
+        config_out_dir = network_dir, # / 'generated_templates',
+        relative_to_dir = None,
+        trop_model="gpt2",
+        trop_dir = None,
+        enable_mongo = False,
+        overrides=(),
+    )
