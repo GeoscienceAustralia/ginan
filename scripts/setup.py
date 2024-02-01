c8cbe7677a510ec099e87221ef97516031a5eefa
Andrew Cleland <andrew.cleland3@gmail.com>
2024-02-01 15:27:45 +1000

    Include yaml template in gn package
    The switch from an explicit "-e ." in requirements.txt to "pip install
    ." caused an issue with the template file no longer being found. This
    change installs the template alongside the package.

--- scripts/setup.py
+++ scripts/setup.py
@@ -4,6 +4,7 @@ setup(
     name="gn",
     version="0.1",
     packages=find_packages(),
+    package_data={"gn": ["templates/*.yaml"]},
     entry_points={
         "console_scripts": [
             "gn=gn.main:cli",
